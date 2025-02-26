545a03b944220aecc95f548ea0200b8b963b45a0
vikram17dp <vikramdp505@gmail.com>
2025-02-26 11:31:23 +0530

    creating an user

---
+++ script.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+async function main() {
+    const user = await prisma.user.create({data:{name:"vikram"}})
+    console.log(user);
+    
+}
+
+main().
+catch(e=>{
+    console.error(e.message)
+})
+.finally(async()=>{
+    await prisma.$disconnect()
+})
